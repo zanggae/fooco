dfcd89b5e080f6646e71dea4051868d67729d5bd
jungwan9 <64017338+jungwan9@users.noreply.github.com>
2020-08-08 12:54:26 +0900

    0808 12:53 DB연동을 위한 lib & xml설정

--- fooco/src/main/java/com/kh/fooco/member/model/dao/MemberDao.java
+++ fooco/src/main/java/com/kh/fooco/member/model/dao/MemberDao.java
@@ -1,8 +1,11 @@
 package com.kh.fooco.member.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository("memberDao")
 public class MemberDao {
-
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
 }
