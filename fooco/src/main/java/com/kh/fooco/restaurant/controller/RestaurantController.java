237fb8a0f0ba51a8afd464bdaa6ce7192660f787
kyeongnamkim <60721649+kyeongnamkim@users.noreply.github.com>
2020-09-01 22:10:49 +0900

    0901 10:10 김경남 발등에 불 떨어짐

--- fooco/src/main/java/com/kh/fooco/restaurant/controller/RestaurantController.java
+++ fooco/src/main/java/com/kh/fooco/restaurant/controller/RestaurantController.java
@@ -1,18 +1,20 @@
 package com.kh.fooco.restaurant.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.URLDecoder;
 import java.util.ArrayList;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.kh.fooco.common.model.vo.PageInfo;
 import com.kh.fooco.restaurant.model.service.RestaurantService;
+import com.kh.fooco.restaurant.model.vo.Restaurant;
 import com.kh.fooco.restaurant.model.vo.Review;
 
 @Controller
@@ -27,8 +29,27 @@ public class RestaurantController {
 	}
 	
 	@RequestMapping("goSearchedRestaurant.do")
-	public String goSearchedRestaurant() {
-		return "restaurant/searchedRestaurant";
+	public ModelAndView goSearchedRestaurant(ModelAndView mv
+									 , @RequestParam(value="page", required=false) Integer page
+									 , @RequestParam(value="location", required=false) String location
+									 , @RequestParam(value="keyword", required=false) String keyword)
+	{
+		int currentPage = 1;
+		
+		if(page != null) {
+			currentPage = page;
+		}
+		
+		ArrayList<Restaurant> list = new ArrayList<Restaurant>();
+		PageInfo pi = new PageInfo();
+		
+		int howManyRestaurant = 0;
+		
+		
+		
+		
+		mv.setViewName("restaurant/searchedRestaurant");
+		return mv;
 	}
 	
 	@RequestMapping("goDetailRestaurant.do")
